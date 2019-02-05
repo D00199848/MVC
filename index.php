18fdbb85fe859e619a6ca1da8374f24506dc2b6f
D00199848 <D00199848@P1107-PC10.ad.dkit.ie>
2019-02-05 15:44:48 +0000

    added new H1 to the index page.

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
+        <h1>Hello Wolrd</h1>
+       
     </body>
 </html>
